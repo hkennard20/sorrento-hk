fa3c9efe7f5100b9a7f0ccdfe6ce04c8e6c44814
hkennard <hughkennard@icloud.com>
2020-11-03 12:30:59 +1100

    changes

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 function onLoad() {
     const btn = document.querySelector('#navbutton')
     const nav = document.querySelector('.navigation')
-
+  
     btn.addEventListener('click', function() {
-        if( nav.classList.contains('open') ) {
-            nav.classList.remove('open')
-        }
-        else {
-            nav.classList.add('open')
-        }
+      if( nav.classList.contains('open') ) {
+        nav.classList.remove('open')
+      }
+      else {
+        nav.classList.add('open')
+      }
     } )
-}
-
-window.addEventListener( 'load' , onload )
+  }
+  
+  window.addEventListener( 'load' , onLoad )
